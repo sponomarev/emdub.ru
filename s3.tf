5e05f9f2936918ce0c6afe82b5952ccbe0a4f1da
Sergey Ponomarev <me@sergey-ponomarev.ru>
2020-06-05 18:20:13 -0400

    Add redirects with S3 buckets

---
+++ s3.tf
@@ -0,0 +1,43 @@
+locals {
+  redirect_to_vk = jsonencode(
+    [
+      {
+        Redirect = {
+          HostName             = "vk.com"
+          HttpRedirectCode     = "301"
+          ReplaceKeyPrefixWith = "e_dub"
+        }
+      },
+    ]
+  )
+}
+
+resource "aws_s3_bucket" "root" {
+  bucket = var.hostname
+
+  acl = "private"
+  website {
+    index_document = "index.html"
+    routing_rules  = local.redirect_to_vk
+  }
+  tags = local.tags
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket" "www" {
+  bucket = "www.${var.hostname}"
+
+  acl = "private"
+  website {
+    index_document = "index.html"
+    routing_rules  = local.redirect_to_vk
+  }
+  tags = local.tags
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
